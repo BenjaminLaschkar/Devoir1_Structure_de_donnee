e29f252b5e49fa2ee5bce000906c57318e7fa7e2
BenjaminLaschkar <benjamin.laschkar@devinci.fr>
2018-05-26 21:24:56 -0400

    Add exercice 4 almost complete

--- Exercice4_Arbre_Genealogique/Personne.h
+++ Exercice4_Arbre_Genealogique/Personne.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <deque>
+
 enum Couleur { Bleu, Marron };
 using namespace std;
 
@@ -8,23 +8,9 @@ class Personne
 public:
 	Personne(const char * nom, const char * prenom, int annee_de_naissance, enum Couleur couleur);
 	~Personne();
-	void AjouterFemme(Personne &femme);
-	void AjouterEnfant(Personne &enfant);
-	void AjouterPere(Personne &pere);
-	void AjouterMere(Personne &mere);
-	Personne * GetFemme();
-	deque<Personne *> GetEnfants();
-	Personne * GetPere();
-	Personne * GetMere();
 	const char * nom;
 	const char * prenom;
 	int annee_de_naissance;
 	Couleur couleur;
-
-private:
-	Personne * femme = nullptr;
-	deque<Personne *> liste_enfant = deque<Personne*>();
-	Personne * pere = nullptr;
-	Personne * mere = nullptr;
 };
 
