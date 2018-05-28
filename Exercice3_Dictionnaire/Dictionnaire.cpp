ccdbd1e9cb26a4c7bce7c7eec90b6132fc3ed3d0
mehdi-fakihani <mehdi.fakihani@outlook.com>
2018-05-28 10:18:45 -0400

    [Exercice 3] : Bug sur la suppression corrigÃ

--- Exercice3_Dictionnaire/Dictionnaire.cpp
+++ Exercice3_Dictionnaire/Dictionnaire.cpp
@@ -116,8 +116,9 @@ Dictionnaire & Dictionnaire::enleverMot(std::string const& mot)
 			if (suite->lettre == 0)  //Idée : Changer, si possible, la signature de la méthode de manière à pouvoir remplacer ce if par suite = suite->enleverMot
 				suite = nullptr;
 
-			if (suite == nullptr && alternative == nullptr)
+			if (suite == nullptr && alternative == nullptr && !endOfWord)
 				lettre = 0;
+
 			else if (suite == nullptr && alternative != nullptr)
 			{
 				lettre = alternative->lettre;
@@ -130,77 +131,19 @@ Dictionnaire & Dictionnaire::enleverMot(std::string const& mot)
 		{
 			alternative->enleverMot(mot);
 			if (alternative->lettre == 0)
-				alternative = nullptr;
+				alternative = std::move(alternative->alternative);
 		}
 	}
 
 	return *this;
 }
 
-/*Dictionnaire & Dictionnaire::enleverMot(std::string const& mot)
-{
-	if (mot.length() == 1)
-	{
-		if (lettre == mot[0] && endOfWord)
-		{
-			if (suite == nullptr && alternative == nullptr)
-			{
-				lettre = 0; //Permet de signifier au parent que le nœud sera à supprimer
-				endOfWord = false;
-			}
-
-			else if (suite == nullptr && alternative != nullptr) //TODO : Écrire un opérateur de transfert pour simplifier le code
-			{
-				lettre = alternative->lettre;
-				endOfWord = alternative->endOfWord;
-				suite = std::move(alternative->suite);
-				alternative = std::move(alternative->alternative);
-			}
-
-			else if (suite != nullptr && alternative != nullptr)
-				endOfWord = false;
-		}
-
-		else if (lettre != mot[0] && alternative != nullptr)
-			alternative->enleverMot(mot);
-
-	}
-
-	else if (mot.length() > 1)
-	{
-		if (lettre == mot[0] && suite != nullptr)
-		{
-			suite->enleverMot(mot.substr(1, mot.length() - 1));
-			if (suite->lettre == 0)  //Idée : Changer, si possible, la signature de la méthode de manière à pouvoir remplacer ce if par suite = suite->enleverMot
-				suite = nullptr;
-
-			if (suite == nullptr && alternative == nullptr)
-				lettre = 0;
-			else if (suite == nullptr && alternative != nullptr)
-			{
-				lettre = alternative->lettre;
-				endOfWord = alternative->endOfWord;
-				suite = std::move(alternative->suite);
-				alternative = std::move(alternative->alternative);
-			}
-		}
-		else if (lettre != mot[0] && alternative != nullptr)
-		{
-			alternative->enleverMot(mot);
-			if (alternative->lettre == 0)
-				alternative = nullptr;
-		}
-	}
-
-	return *this;
-}*/
-
 void Dictionnaire::afficher() const
 {
-	std::cout << "DEBUT D'AFFICHAGE" << std::endl;
+	std::cout << "DEBUT D'AFFICHAGE DU DICTIONNAIRE" << std::endl;
 	std::string tampon = "";
 	afficher(tampon);
-	std::cout << "FIN D'AFFICHAGE" << std::endl;
+	std::cout << "FIN D'AFFICHAGE DU DICTIONNAIRE" << std::endl;
 }
 
 void Dictionnaire::afficher(std::string& tampon) const
